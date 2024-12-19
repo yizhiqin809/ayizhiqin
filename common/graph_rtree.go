fa7270b8ff5f2fe7054f98a7379cb1eed0b43529
yizhiqin809 <ayizhiqin@163.com>
2024-12-19 16:34:02 +0800

    Update graph_rtree.go

--- common/graph_rtree.go
+++ common/graph_rtree.go
@@ -6,14 +6,14 @@ import (
 	"math"
 )
 
-func RtreegoRect(r Rectangle) rtreego.Rect {
+func RtreegoRect(r Rectangle) *rtreego.Rect {
 	dx := math.Max(0.00000001, r.Max.X - r.Min.X)
 	dy := math.Max(0.00000001, r.Max.Y - r.Min.Y)
 	rect, err := rtreego.NewRect(rtreego.Point{r.Min.X, r.Min.Y}, []float64{dx, dy})
 	if err != nil {
 		panic(err)
 	}
-	return rect
+	return &rect
 }
 
 type edgeSpatial struct {
@@ -26,8 +26,9 @@ func (e *edgeSpatial) Bounds() *rtreego.Rect {
 		r := e.edge.Src.Point.Rectangle()
 		r = r.Extend(e.edge.Dst.Point)
 		e.rect = RtreegoRect(r)
+		return rect
 	}
-	return e.rect
+	return *e.rect
 }
 
 type Rtree struct {
@@ -35,10 +36,14 @@ type Rtree struct {
 }
 
 func (rtree Rtree) Search(rect Rectangle) []*Edge {
-	spatials := rtree.tree.SearchIntersect(RtreegoRect(*rect))
+	spatials := rtree.tree.SearchIntersect(*RtreegoRect(*rect))
 	edges := make([]*Edge, len(spatials))
 	for i := range spatials {
-		edges[i] = spatials[i].(edgeSpatial).edge
+		if spatial, ok := spatials[i].(edgeSpatial); ok {
+            edges[i] = spatial.edge
+			}else{
+			continue
+		}
 	}
 	return edges
 }
